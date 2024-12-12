ffd86ec7508ce3885a9ad5849c26ad7c09335dc9
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-12-12 23:27:10 +0530

    adding files

---
+++ backend/models/user.model.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+c
+
+const userSchema = new mongoose.Schema({
+    fullname: {
+        firstname: {
+            type: String,
+            required: true,
+            minlength: [3, "First name must be at least 3 characters long"]
+        },
+        lastname: {
+            type: String,
+            minlength: [3, "Last name must be at least 3 characters long"]
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: [5, "Email must be at least 5 characters long"]
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    socketId: {
+        type: String,
+    }
+});
