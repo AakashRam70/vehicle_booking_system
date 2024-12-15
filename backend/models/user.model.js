20fe811f21ea2385b6960603d6938b40840adc9a
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-12-15 23:42:11 +0530

    modifying uuser.model.js

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -34,3 +34,7 @@ userSchema.method.generateAuthToken = function () {
     const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET);
     return token;
 }
+
+userSchema.method.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
