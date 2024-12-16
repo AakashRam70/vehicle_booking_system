6c6792821d7e2fd2b443c532a1e996558d17bfa7
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-12-16 22:24:11 +0530

    modifying user.routes.js and user.controller.js

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const userModel = require("../models/user.model");
 const router = express.Router();
 
 
-
+router.post('/register', [
+    body('email').isEmail().withMessage('Invalid email'),
+    body("fullname").isLength({ min: 3 }).withMessage("Fullname must be at least 3 characters long"),
+    body("password").isLength({ min: 6 }).withMessage("Password must be at least 6 characters long"),
+],
+    UserController.registerUser
+);
 
 
 module.exports = router;
